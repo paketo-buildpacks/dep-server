d3b56968fffc0b950503e194a6868bbccc18adb8
Sophie Wigmore <swigmore@vmware.com>
2021-07-22 15:30:08 +0000

    unescape purl path
    Signed-off-by: Forest Eckhardt <feckhardt@pivotal.io>

--- pkg/dependency/purl/purl.go
+++ pkg/dependency/purl/purl.go
@@ -1,6 +1,8 @@
 package purl
 
 import (
+	"net/url"
+
 	"github.com/package-url/packageurl-go"
 )
 
@@ -23,5 +25,15 @@ func (PURLGenerator) Generate(name, version, sourceSHA, source string) string {
 		"",
 	)
 
-	return purl.ToString()
+	// Unescape the path to remove the added `%2F` and other encodings added to
+	// the URL by packageurl-go
+	// If the unescaping fails, we should still return the path URL with the
+	// encodings, packageurl-go has examples with both the encodings and without,
+	// we prefer to avoid the encodings when we can for convenience.
+	purlString, err := url.PathUnescape(purl.ToString())
+	if err != nil {
+		return purl.ToString()
+	}
+
+	return purlString
 }
