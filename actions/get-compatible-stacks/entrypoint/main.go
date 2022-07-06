1d4d16dcc8854c43f74a95f053963b5f5bebb0fb
Frankie Gallina-Jones <frankieg@vmware.com>
2022-07-06 15:54:21 -0400

    create action that finds stacks thats that match versions

---
+++ actions/get-compatible-stacks/entrypoint/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/Masterminds/semver"
+)
+
+type Stack struct {
+	ID                string   `json:"id"`
+	VersionConstraint string   `json:"version-constraint,omitempty"`
+	Mixins            []string `json:"mixins,omitempty"`
+}
+
+func main() {
+	var config struct {
+		Version    string
+		StacksJSON string
+	}
+
+	flag.StringVar(&config.StacksJSON,
+		"stacks",
+		"",
+		"JSON array of stack IDs with version constraints")
+	flag.StringVar(&config.Version,
+		"version",
+		"",
+		"Version of dependency")
+
+	flag.Parse()
+
+	if config.StacksJSON == "" {
+		config.StacksJSON = `[]`
+	}
+
+	var stacks []Stack
+	err := json.Unmarshal([]byte(config.StacksJSON), &stacks)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var result []Stack
+	for _, stack := range stacks {
+		if stack.VersionConstraint == "" {
+			result = append(result, Stack{ID: stack.ID, Mixins: stack.Mixins})
+			continue
+		}
+		c, err := semver.NewConstraint(stack.VersionConstraint)
+		if err != nil {
+			log.Fatal(err)
+		}
+		v, _ := semver.NewVersion(config.Version)
+		if err != nil {
+			log.Fatal(err)
+		}
+		compatible, msgs := c.Validate(v)
+		if compatible {
+			result = append(result, Stack{ID: stack.ID, Mixins: stack.Mixins})
+			continue
+		}
+		for _, m := range msgs {
+			log.Println(m)
+		}
+	}
+	output, err := json.Marshal(result)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if len(result) == 0 {
+		log.Fatal("No stacks compatible with this version")
+	}
+
+	log.Println("Output: ", string(output))
+	fmt.Printf("::set-output name=compatible-stacks::%s\n", string(output))
+}
